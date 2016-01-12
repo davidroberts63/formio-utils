c61e7c20336be3047bebce1de0ffc3df77a8ec20
Rahat Ahmed <rahatarmanahmed@gmail.com>
2016-01-12 09:03:19 -0600

    Add getComponent and flattenComponents

--- src/index.js
+++ src/index.js
@@ -1,27 +1,62 @@
 module.exports = {
-	eachComponent: function eachComponent(components, fn) {
-		if (!components) return;
+  /**
+   * Iterate through each component within a form.
+   * @param components
+   * @param fn
+   */
+  eachComponent: function eachComponent(components, fn) {
+    if (!components) return;
 
-		components.forEach(function(component) {
-			if (component.columns) {
-				component.columns.forEach(function(column) {
-					eachComponent(column.components, fn);
-				});
-			}
+    components.forEach(function(component) {
+      if (component.columns) {
+        component.columns.forEach(function(column) {
+          eachComponent(column.components, fn);
+        });
+      }
 
-			else if (component.rows) {
-				[].concat.apply([], component.rows).forEach(function(row) {
-					eachComponent(row.components, fn);
-				});
-			}
+      else if (component.rows) {
+        [].concat.apply([], component.rows).forEach(function(row) {
+          eachComponent(row.components, fn);
+        });
+      }
 
-			else if (component.components) {
-				eachComponent(component.components, fn);
-			}
+      else if (component.components) {
+        eachComponent(component.components, fn);
+      }
 
-			else {
-				fn(component);
-			}
-		});
-	}
+      else {
+        fn(component);
+      }
+    });
+  },
+
+  /**
+   * Get a component by its key
+   * @param components
+   * @param key The key of the component to get
+   * @returns The component that matches the given key, or undefined if not found.
+   */
+  getComponent: function getComponent(components, key) {
+    var result;
+    this.eachComponent(components, function(component) {
+      if (component.key === key) {
+        result = component;
+      }
+    });
+    return result;
+  },
+
+  /**
+   * Flatten the form components for data manipulation.
+   * @param components
+   * @param flattened
+   * @returns {*|{}}
+   */
+  flattenComponents: function flattenComponents(components) {
+    var flattened = {};
+    this.eachComponent(components, function(component) {
+      flattened[component.key] = component;
+    });
+    return flattened;
+  }
 };
--- test/test.js
+++ test/test.js
@@ -1,16 +1,43 @@
 /* eslint-env mocha */
 var expect = require('chai').expect;
+var writtenNumber = require('written-number');
 
 var utils = require('../src/index');
 
-describe('eachComponent', function() {
-	var components = require('./components.json');
+var components = require('./components.json');
 
+describe('eachComponent', function() {
 	it('should iterate through nested components in the right order', function() {
 		var n = 1;
 		utils.eachComponent(components, function(component) {
 			expect(component.order).to.equal(n);
 			n += 1;
 		});
+	});
+});
+
+describe('getComponent', function() {
+	it('should return the correct components', function() {
+		for (var n = 1; n <= 8; n += 1) {
+			var component = utils.getComponent(components, writtenNumber(n));
+			expect(component).not.to.be.null;
+			expect(component).not.to.be.undefined;
+			expect(component).to.be.an('object');
+			expect(component.order).to.equal(n);
+			expect(component.key).to.equal(writtenNumber(n));
+		}
+	});
+});
+
+describe('flattenComponents', function() {
+	it('should return an object of flattened components', function() {
+		var flattened = utils.flattenComponents(components);
+		for (var n = 1; n <= 8; n += 1) {
+			var component = flattened[writtenNumber(n)];
+			expect(component).not.to.be.undefined;
+			expect(component).to.be.an('object');
+			expect(component.order).to.equal(n);
+			expect(component.key).to.equal(writtenNumber(n));
+		}
 	});
 });
